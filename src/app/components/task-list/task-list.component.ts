71ff0766c8cd0a9fbdad7ce4eed0380c7c13379e
Helio Gonzaga Santos <helio.santosjunior@outlook.com>
2022-02-22 14:55:33 -0300

    Created textarea for edit task

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 interface TaskList {
-  label: string;
-  done: boolean;
-  folderName: string;
+  label: string
+  done: boolean
+  folderName: string,
+  editing: boolean
 }
 
 @Component({
@@ -15,52 +16,66 @@ export class TaskListComponent implements OnInit {
 
   constructor() { }
 
+  showInputEditTask = ''
+
   tasksList: TaskList[] = [
     {
       label: 'Created module scmodule',
       done: false,
-      folderName: 'Banco Safra'
+      folderName: 'Banco Safra',
+      editing: false
     },
     {
       label: 'Created module scmodule',
       done: false,
-      folderName: 'Banco Safra'
+      folderName: 'Banco Safra',
+      editing: false
     },
     {
       label: 'Created module scmodule',
       done: false,
-      folderName: 'Banco Safra'
+      folderName: 'Banco Safra',
+      editing: false
     },
     {
-      label: 'Created module Dicta velit suscipit, fugit, illum possimus',
+      label: 'Created module Dicta velit suscipit fugit illum possimus',
       done: false,
-      folderName: 'Banco Safra'
-    },
-    {
-      label: 'Created module scmodule',
-      done: false,
-      folderName: 'Banco Safra'
-    },
-    {
-      label: 'Created module Dicta velit suscipit, fugit, illum possimus',
-      done: false,
-      folderName: 'Banco Safra'
+      folderName: 'Banco Safra',
+      editing: false
     },
     {
       label: 'Created module scmodule',
       done: false,
-      folderName: 'Banco Safra'
+      folderName: 'Banco Safra',
+      editing: false
+    },
+    {
+      label: 'Created module Dicta velit suscipit',
+      done: false,
+      folderName: 'Banco Safra',
+      editing: false
     },
     {
       label: 'Created module scmodule',
       done: false,
-      folderName: 'Banco Safra'
+      folderName: 'Banco Safra',
+      editing: false
+    },
+    {
+      label: 'Created module scmodule',
+      done: false,
+      folderName: 'Banco Safra',
+      editing: false
     },
   ]
 
   doneTask = (task: TaskList) => {
     task.done = !task.done
   }
+
+  editTask = (task: TaskList) => {
+    task.editing = !task.editing
+  } 
 
   ngOnInit(): void {
   }
